f8bcd47147f3d15fd703c2660c127be020ffb115
Jan Sykora <jan.sykora@gooddata.com>
2016-11-17 20:34:17 +0100

    skeleton

---
+++ sit-aplikace/src/main/java/io/jansyk13/testlink/app/SitAplikaceApplication.java
@@ -0,0 +1,12 @@
+package io.jansyk13.testlink.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitAplikaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitAplikaceApplication.class, args);
+	}
+}
